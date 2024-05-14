d04b55e9da41d1901495cb28067af3b150dd1913
guionwind <95938918+guionwind@users.noreply.github.com>
2024-05-14 16:16:21 +0200

    creating angular components

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-server',
+    templateUrl: './server.component.html',
+    //styleUrl: './server.component.css'
+    styles: [`h4 {color: green;}`, `h3 { color: purple; }`]
+})
+export class ServerComponent {
+
+}
---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  //template: `<h3>un dos tres</h3>`,
+  styleUrl: './servers.component.css'
+})
+export class ServersComponent {
+
+}
