548a05b28ba90fbaab12493b0eb17e2be52fa691
Sheng Ye <95938918+guionwind@users.noreply.github.com>
2024-05-15 18:32:06 +0200

    basic elements of datos-vehiculo component

--- src/app/datos-vehiculo/datos-vehiculo.component.ts
+++ src/app/datos-vehiculo/datos-vehiculo.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
@@ -5,11 +6,34 @@ import { FormsModule } from '@angular/forms';
   selector: 'app-datos-vehiculo',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './datos-vehiculo.component.html',
   styleUrl: './datos-vehiculo.component.css'
 })
 export class DatosVehiculoComponent {
 
+  submitted: boolean = false;
+
+  tipoVehiculo: string = "";
+  numeroBastidor: string = "";
+  marca: string = "";
+  modelo: string = "";
+  precioCompra: string = "";
+  fechaCompra: string = "";
+
+  asistenciaCarretera: boolean = false;
+  responsabilidadCivil: boolean = false;
+  vehiculoSustitucion: boolean = false;
+  colisionAnimales: boolean = false;
+
+  constructor() {
+    this.submitted = false;
+  }
+
+  onClick(event: any) {
+    console.log(event)
+    this.submitted = true;
+  }
 }
