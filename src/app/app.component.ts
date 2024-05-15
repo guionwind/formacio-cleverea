e72a827f0ffad0561e796508dfbe1caaa068ac35
Sheng Ye <95938918+guionwind@users.noreply.github.com>
2024-05-15 17:11:41 +0200

    create datos-vehiculo component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DatosVehiculoComponent } from './datos-vehiculo/datos-vehiculo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatosVehiculoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/datos-vehiculo/datos-vehiculo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-datos-vehiculo',
+  standalone: true,
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './datos-vehiculo.component.html',
+  styleUrl: './datos-vehiculo.component.css'
+})
+export class DatosVehiculoComponent {
+
+}
