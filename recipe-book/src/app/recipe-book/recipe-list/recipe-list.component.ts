cbe6af5ddacf1375f1ab4b889839fc136894a765
Sheng Ye <95938918+guionwind@users.noreply.github.com>
2024-05-17 12:03:31 +0200

    section 3 - the basics completed
    Creation of the main components of the webpage, pending implementation of data sharing between components

--- recipe-book/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipe-book/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('Spaghetti', 'Test recipe', 'https://media.istockphoto.com/id/652225084/es/foto/espagueti-bolo%C3%B1esa-en-un-plato-blanco.jpg?s=2048x2048&w=is&k=20&c=JI8WtY5mZtC9x3i7c50wkAkeNQLfWmqYx1iEINYc0CY='),
+    new Recipe('Spaghetti', 'Test recipe', 'https://media.istockphoto.com/id/652225084/es/foto/espagueti-bolo%C3%B1esa-en-un-plato-blanco.jpg?s=2048x2048&w=is&k=20&c=JI8WtY5mZtC9x3i7c50wkAkeNQLfWmqYx1iEINYc0CY='),
+  ];
 
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
